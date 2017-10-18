dc568aa57db0b87ac6f763c32e45eae0d4c2430d
unknown <mstange22@gmail.com>
2017-10-17 21:46:25 -0700

    build

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -108,7 +108,7 @@ module.exports = function(app) {
         });
     });
 
-    app.get("/", function(req, res) {
+    app.get("*", function(req, res) {
         res.sendFile(path.join(__dirname, "../client/build/index.html"));
     });
 }
